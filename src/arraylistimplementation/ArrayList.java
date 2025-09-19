56f303863e1c6104dfe0e8f5b8648b83d5a45c3e
Ganesh Jadhav <jadhav.ganesh1700@gmail.com>
2025-09-19 19:20:53 +0530

    implementing arraylist according to the java arraylist design a fetature for the leftShift righshift

--- src/arraylistimplementation/ArrayList.java
+++ src/arraylistimplementation/ArrayList.java
@@ -1,8 +1,8 @@
 package arraylistimplementation;
 
-import java.util.Arrays;
 
-public class ArrayList {
+
+public class ArrayList<E> {
 	int count=0;
 	int initCap=ResizableArray.intialCap;
 	Object ar[];
@@ -13,14 +13,17 @@ public class ArrayList {
 		
 	}
     ArrayList(int intialCap){
-    	this.initCap=intialCap;
-    	ar=new Object[initCap];
+    	if(intialCap>0) {
+    		this.initCap=intialCap;
+        	ar=new Object[initCap];
+    	}
 		
 	}
     
-    public boolean add(Object o){
+    public boolean add(E o){
     	if(count<ar.length*ResizableArray.loadFactor) {
     		ar[count++]=o;
+    		
     	}
     	else {
     		Object temp[]= new Object[ar.length*2];
@@ -31,17 +34,79 @@ public class ArrayList {
     		ar=temp;
     		
     	}
+    
     	return true;
     		
     	}
     
     
+    public boolean add(int index,E o) {
+    	if(index>=0 && index<=count) {
+    		rightShift(index);
+    		ar[index]=o;
+    		count++;
+    	}
+    	else {
+    		return false;
+    	}
+    	
+    	
+    	return true;
+    	}
+    public boolean rightShift(int index) {
+    	
+    	for(int i=count-1;i>=index;i--) {
+    		
+    		ar[i+1]=ar[i];
+    		
+    		}
+    	return true;
+    	
+    }
+     public boolean leftShift(int index) {
+    	
+    	if(index>=0 && index<count) {
+    		for(int i=index;i<count-1;i++) {
+        		
+        		ar[i]=ar[i+1];
+        		
+        		}
+        	ar[count-1]=null;
+        	count--;
+        	
+    	}
+    	else {
+    		return false;
+    	}
+    	return true;
+    	
+    }
+    
+    
     
      @Override
     public String toString() {
-		return Arrays.toString(ar);
+		String op="[";
+		for(int i=0;i<count;i++) {
+			if(i<count-1) {
+				op+=ar[i]+ ","; 
+			}
+			else {
+				op+=ar[i];
+			}
+	
+	}
+		op+= "]";
+		return op;
     	
     }
+     
+     public  E get(int index) {
+    	 if(index>=0 && index <count) {
+    		 return (E) ar[index]; 
+    	 }
+    	 else return null;
+     }
     	
 
 }
--- src/arraylistimplementation/ArrayListDriver.java
+++ src/arraylistimplementation/ArrayListDriver.java
@@ -3,19 +3,14 @@ package arraylistimplementation;
 public class ArrayListDriver {
 
 	public static void main(String[] args) {
-		ArrayList a1 =new ArrayList();
+		ArrayList<String> a1 =new ArrayList<String>();
 		a1.add("Ganesh");
-		a1.add("Ganesh");
-		a1.add("Ganesh");
-		a1.add("Ganesh");
-		a1.add("Ganesh");
-		a1.add("Ganesh");
-		a1.add("Ganesh");
-		a1.add("Ganesh");
-		a1.add("Ganesh");
-		a1.add("Ganesh");
-		a1.add("Ganesh");
-		System.out.print(a1);
+		a1.add("Staish");
+		a1.add("Jadhav");
+		a1.leftShift(0);
+		System.out.println(a1.get(1));
+	   
+		
 		
 	}
 
